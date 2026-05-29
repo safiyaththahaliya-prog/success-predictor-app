eca51ecc5ec018bc8652739ea2f9d776a070f55c
safiyaththahaliya-prog <safiyaththahaliya@gmail.com>
2026-05-29 13:35:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 import streamlit as st
-import joblib
-import numpy as np
-
-model = joblib.load("success_model.pkl")
 
 st.title("🎬 Success Predictor")
 
-rating = st.number_input("Movie Rating", 0.0, 10.0)
+rating = st.slider("Movie Rating", 0.0, 10.0, 5.0)
 
-votes = st.number_input("Votes", 0)
+votes = st.number_input("Votes", min_value=0)
 
 if st.button("Predict"):
-
-    data = np.array([[rating, votes]])
-
-    prediction = model.predict(data)
-
-    st.success(f"Prediction: {prediction[0]}")
+    st.success("App is working successfully!")
