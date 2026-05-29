05c5a642b19644d690dcd50a5c0c843d4f9d2c86
safiyaththahaliya-prog <safiyaththahaliya@gmail.com>
2026-05-29 14:43:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import joblib
-import numpy as np
 
 model = joblib.load("success_model.pkl")
 
-st.title("Movie Success Predictor")
+st.title("🎬 Movie Success Predictor")
 
-feature1 = st.number_input("Enter startYear")
+st.write("Predict movie rating based on movie details.")
+
+startYear = st.number_input("Start Year", 1900, 2030, 2020)
+runtimeMinutes = st.number_input("Runtime Minutes", 60, 300, 120)
+numVotes = st.number_input("Number of Votes", 0, 1000000, 1000)
+rank = st.number_input("Movie Rank", 1, 100, 50)
 
 if st.button("Predict"):
-    prediction = model.predict([[feature1]])
-    st.success(f"Predicted Rating: {prediction[0]}")
+
+    prediction = model.predict([[startYear]])
+
+    st.success(f"Predicted Rating: {prediction[0]:.2f}")
