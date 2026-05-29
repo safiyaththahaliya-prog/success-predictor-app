5396a9ec78173cbbbbb821fdf4e0a9a058bd9e1a
safiyaththahaliya-prog <safiyaththahaliya@gmail.com>
2026-05-29 13:27:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ import streamlit as st
 import joblib
 import numpy as np
 
-# Load saved model
 model = joblib.load("success_model.pkl")
 
 st.title("🎬 Success Predictor")
 
-rating = st.number_input("Average Rating", min_value=0.0, max_value=10.0)
+rating = st.number_input("Movie Rating", 0.0, 10.0)
 
-votes = st.number_input("Number of Votes", min_value=0)
+votes = st.number_input("Votes", 0)
 
 if st.button("Predict"):
 
